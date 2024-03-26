c146298451d155c5433cdab956ad26d51510ff4f
Suraj Govind <surajgovind332000@gmail.com>
2024-03-26 14:39:10 +0530

    fix: deep equal before delayed val enqueue

--- src/hooks/useDelayedValue.ts
+++ src/hooks/useDelayedValue.ts
@@ -1,12 +1,19 @@
 import { useEffect, useState } from "react";
+import equal from "fast-deep-equal/react";
 
 export const useDelayedValue = <T>(val: T, delayMs: number) => {
   const [data, setData] = useState(val);
 
-  useEffect(() => {
+  const enqueueChange = () => {
     setTimeout(() => {
       setData(val);
     }, delayMs);
+  };
+
+  useEffect(() => {
+    const isSame = equal(val, data);
+    if (!isSame) enqueueChange();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [val]);
 
